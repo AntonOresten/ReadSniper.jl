be8261867f99c0b5e5ebcb5e257ab4ec152a6529
Periareion <periareion05@gmail.com>
2023-02-05 02:36:47 +0100

    Removed faidx

--- src/iteration.jl
+++ src/iteration.jl
@@ -1,6 +1,6 @@
 
 
-function parallel_scanning(
+function scattered_scanning(
     config::Config,
     reference::Reference,
     datafile_list::Vector{DatafileMetadata},
@@ -18,33 +18,34 @@ function parallel_scanning(
     for (file_number, datafile) in collect(enumerate(datafile_list))
         if show_progress update(progress_bar, file_number-1, sum(reads_scanned), sum(length(rs) for rs in read_result_sets)) end
 
-        idx = faidx(open(datafile.path, "r"))
-        readers = [FASTAReader(open(datafile.path, "r"), index=idx, copy=false) for _ in 1:config.nthreads]
+        readers = [FASTAReader(open(datafile.path, "r"), copy=false) for _ in 1:config.nthreads]
         number_of_readers = length(readers)
         reader_window_size = datafile.read_count ÷ number_of_readers
 
         Threads.@threads for (i, reader) in collect(enumerate(readers))
-            window_start = (i - 1) * reader_window_size + 1
-            window_end = i * reader_window_size + (i == number_of_readers ? datafile.read_count % number_of_readers : 0)
-            seekrecord(reader, window_start)
-            for read_number in window_start:window_end
+            for _ in 1:i-1
+                if !eof(reader) first(reader) end
+            end
+            read_index = i - 1
+            while !eof(reader)
+                read_index += 1
                 record = first(reader)
+                if !iszero((read_index - 1) % config.nthreads - (i - 1)) continue end
+
                 reads_scanned[i] += 1
 
-                ref_range_start, ref_range_end, score = scan_read(config, record, reference.kmer_dict)
+                score, ref_range_start, ref_range_end = scan_read(config, record, reference.kmer_dict)
                 increment_dict_value!(score, 1, score_frequency_dicts[i])
                 if score >= config.threshold
-                    push!(read_result_sets[i], ReadResult(read_number, ref_range_start, ref_range_end, score))
+                    push!(read_result_sets[i], ReadResult(read_index, ref_range_start, ref_range_end, score))
                 end
 
-                if show_progress && i == 1 && iszero(read_number % 729)
+                if show_progress && i == 1 && iszero(read_index % 137)
                     update(progress_bar, file_number-1, sum(reads_scanned), sum(length(rs) for rs in read_result_sets))
                 end
             end
+            close(reader)
         end
-        for reader in readers close(reader) end
-        idx = nothing
-        GC.gc()
     end
 
     for sfd in score_frequency_dicts
@@ -67,7 +68,7 @@ function analyse_dataset(
 )
     score_frequency_dict = Dict{Int32, Int32}()
 
-    read_result_sets = parallel_scanning(config, reference, datafile_list, score_frequency_dict, show_progress)
+    read_result_sets = scattered_scanning(config, reference, datafile_list, score_frequency_dict, show_progress)
     read_result_SOA = sort!(StructArray(vcat([collect(rr_set) for rr_set in read_result_sets]...)), by=result->result.score, rev=true)
 
     sorted_match_bins = sort!(collect(score_frequency_dict), by=pair->pair[1])
--- src/reads.jl
+++ src/reads.jl
@@ -1,7 +1,7 @@
 
 
 struct ReadResult
-    read_number::Int32
+    read_index::Int32
     ref_range_start::Int32
     ref_range_end::Int32
     score::Int32
@@ -18,31 +18,14 @@ function scan_read(
     read_sequence::LongDNA{4} = sequence(LongDNA{4}, record)
     match_indices::Vector{Int} = vcat(kmer_match_indices(read_sequence, kmer_dict, config.k, config.step)...)
 
-    if length(match_indices) == 0
-        return (0, 0, 0)
-    end
+    if length(match_indices) == 0 return (0, 0, 0) end
 
     lise = LISE(match_indices)
     score::Int64, range_start::Int64, range_end::Int64 = max_subseq_in_range(lise, read_length)
     ref_range_start::Int64 = range_start == 0 ? 0 : lise[range_start]
     ref_range_end::Int64 = range_end == 0 ? 0 : lise[range_end]
 
-    #=if ref_range_start == -769 && ref_range_end == -545
-        scatter(
-            match_indices,
-            1:length(match_indices),
-            tickfont = font(10, "Computer Modern"),
-            guidefont = font(12, "Computer Modern"),
-            xlabel="Position in reference",
-            formatter=:plain,
-            legend = false,
-            fmt = :svg,
-            margins = 0.4Plots.cm,
-        )
-        savefig("read.svg")
-    end=#
-
-    return (ref_range_start * !(ref_range_start < 0 < ref_range_end), ref_range_end, config.step*score)
+    return config.step*score, ref_range_start * !(ref_range_start < 0 < ref_range_end), ref_range_end
 end
 
 export scan_read
@@ -60,7 +43,7 @@ export scan_read
 
     # for mw in matched_windows
     #     score = count(view(bool_match_vector, mw[1]:mw[2]))
-    #     push!(read_results, ReadResult(Int32(read_number), Int32(mw[1]), Int32(mw[2]), Int32(config.step*score)))
+    #     push!(read_results, ReadResult(Int32(read_index), Int32(mw[1]), Int32(mw[2]), Int32(config.step*score)))
     # end
 
     # read_results
